7410490c5f12ca38bf161631693f781ad0d510c5
crylonelyboy <arniusss@yahoo.com>
2018-05-03 20:52:32 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+
+$to = "arniusss@yahoo.com";
+
+if (isset($_POST['submit']))
+{
+    #Тут делаем проверки и выводим ошибки если, что то не так
+}
+else #Если ошибок не обнаружено переходим созданию письма
+{
+     $subject = "Order";
+    $message = "Firstname: " . $_POST['fname'];
+	$message = "Lastname: " . $_POST['lname'];
+    $message .= "<br>Message: " . $_POST['message'];
+    $message .= "<br>Email: " . $_POST['email'];
+ 
+    $headers  = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
+    $headers .= "From: " . $_POST['fname'] . " <" . $_POST['email'] . ">". $_POST['message'] ."\r\n";
+ 
+    mail($to, $subject, $message, $headers);
+    if (!mail($to, $subject, $message, $headers))
+    {
+     #Вышла ошибка, сообщение не ушло
+    }
+    else
+    {
+     #Все ок!
+    }
+}
+?>
